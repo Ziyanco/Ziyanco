fac5d5c4676ca21d040a06e7ffaf8f92973c06a5
1 <lzy_3001@163.com>
2024-09-14 18:05:32 +0800

    fixed:表名

--- src/Extends/functions.php
+++ src/Extends/functions.php
@@ -235,6 +235,40 @@ if (!function_exists('snakeToCamel')) {
         return $ucfirst ? ucfirst($str) : $str;
     }
 }
+if (! function_exists('blank')) {
+    /**
+     * 判断给定的值是否为空.
+     */
+    function blank(mixed $value): bool
+    {
+        if (is_null($value)) {
+            return true;
+        }
 
+        if (is_string($value)) {
+            return trim($value) === '';
+        }
+
+        if (is_numeric($value) || is_bool($value)) {
+            return false;
+        }
+
+        if ($value instanceof Countable) {
+            return count($value) === 0;
+        }
+
+        return empty($value);
+    }
+}
+
+if (! function_exists('filled')) {
+    /**
+     * 判断给定的值是否不为空.
+     */
+    function filled(mixed $value): bool
+    {
+        return ! blank($value);
+    }
+}
 
 
